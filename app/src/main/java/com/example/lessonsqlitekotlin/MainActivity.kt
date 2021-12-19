2302242a74212e18f51c67be05e8bc62196b8bb4
AlexeySkillman <Alexey.skillman@gmail.com>
2021-12-19 12:21:11 +0500

    Created UI elements for Main and Edit Activities

--- app/src/main/java/com/example/lessonsqlitekotlin/MainActivity.kt
+++ app/src/main/java/com/example/lessonsqlitekotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lessonsqlitekotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
@@ -14,33 +15,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        onClickSave()
-
-        tvText.setMovementMethod(ScrollingMovementMethod());
     }
 
     override fun onResume(){
         super.onResume()
         myDbManager.openDb()
         val dataList = myDbManager.readDbData()
-        for (item in dataList){
-            tvText.append(item)
-            tvText.append("\n")
-        }
     }
 
 
-    private fun onClickSave(){
-        onClickSave.setOnClickListener(){
-            tvText.text = ""
-            // myDbManager.openDb()
-            myDbManager.insertDb(edTitle.text.toString(), edContent.text.toString())
-            val dataList = myDbManager.readDbData()
-            for (item in dataList){
-                tvText.append(item)
-                tvText.append("\n")
-            }
-        }
+    fun onClickNew(view: View){
+        val i  = Intent(this, EditActivity::class.java)
+        startActivity(i)
     }
 
     override fun onDestroy() {
