720c9a14deee5cf83f02bc992ef1d82bf1474712
AlexeySkillman <Alexey.skillman@gmail.com>
2021-12-21 16:27:09 +0500

    Added Image UI elements and set Listeners on Upload and Delete Image

--- app/src/main/java/com/example/lessonsqlitekotlin/EditActivity.kt
+++ app/src/main/java/com/example/lessonsqlitekotlin/EditActivity.kt
@@ -1,11 +1,25 @@
 package com.example.lessonsqlitekotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.edit_activity.*
 
 class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.edit_activity)
+    }
+
+    fun onClickAddImage(view: View){
+        mainImageLayout.visibility = View.VISIBLE
+        fbAddImage.visibility = View.GONE
+    }
+    fun onClickDeleteImage(view: View){
+        mainImageLayout.visibility = View.GONE
+        fbAddImage.visibility = View.VISIBLE
+    }
+    fun onClickEditImage(view: View){
     }
 }
